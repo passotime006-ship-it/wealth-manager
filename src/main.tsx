488c573b4e6cafbbb939232eee4ca56a5e929a21
Vraj <passotime006@example.com>
2026-07-13 19:22:35 +0000

    Use HashRouter to fix GitHub Pages refresh 404

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import App from './App';
 import { AuthProvider } from './context/AuthContext';
 import { ThemeProviderWrapper } from './context/ThemeContext';
@@ -9,9 +9,9 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ThemeProviderWrapper>
       <AuthProvider>
-        <BrowserRouter basename="/wealth-manager">
+        <HashRouter>
           <App />
-        </BrowserRouter>
+        </HashRouter>
       </AuthProvider>
     </ThemeProviderWrapper>
   </React.StrictMode>
